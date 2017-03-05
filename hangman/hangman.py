e78bc5dab2c85ccf02a9fc9cd1715b0231fd6afb
Navin Kumar <kumarn@advisory.com>
2017-03-05 01:18:39 -0500

    hangman version 2

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,59 +1,76 @@
 import random
 import requests
+import sys
 
 def chooseAWord():
 	wordSite = "https://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
 	response = requests.get(wordSite)
-	wordDictionary = response.content.splitlines()
-	#wordDictionary = ['sun', 'moon', 'earth', 'jupiter', 'mercury', 'neptune', 'mars']
-	print("Work dictionary: ", wordDictionary[1].decode('UTF-8'), len(wordDictionary[1]))
+	wordDictionary = response.content.decode('UTF-8').splitlines()
+	print(wordDictionary)
 
-	chosenWord = random.choice(wordDictionary).decode('UTF-8')
-	#chosenWord = wordDictionary[random.randrange(0, len(wordDictionary), 1)]
-	print("Word randomly generated for the game...!")
-	print("Word to guess is: ", chosenWord)
-	printChosenWord(chosenWord)
+	chosenWord = random.choice(wordDictionary).upper()
+	#print("Word randomly generated for the game is:", chosenWord)
+	return chosenWord
 
-	chosenWordList = list(chosenWord)
-	print(chosenWordList)
+def initializeGuessedWord(lenWord):
+	guessedLetters = ["_"] * lenWord
+	return guessedLetters
 
-def printChosenWord(chosenWord):
-	for letter in chosenWord:
-		print("- "),
+def printWordFormatted(letters, doMask=False):
+	print("********************************")
+	sys.stdout.write("Chosen word: " if doMask else "Guessed word: ")
+	for letter in letters:
+		#print("letter:", letter)
+		sys.stdout.write('X' if doMask else letter)
 
-def initializeGuessedWord():
-	guessedWordList = ["_"] * len(chosenWord)
-print("guessed word is",guessedWordList)
+
+###########################
+### GAME STARTS HERE ######
+###########################
 
 print("Welcome to Hangman game!!!")
 
-maxAttempts = 0
+maxAttempts = 6
 numOfAttempts = 0
 answer = False
-for i in range(0,len(chosenWordList)):
-	print (i, chosenWord[i])
 print("You will have total " + str(maxAttempts) + " attempts to correctly guess the word.")
 
-chooseAWord()
+chosenWord = chooseAWord()
+chosenLetters = list(chosenWord)
+lenChosenWord = len(chosenWord)
+printWordFormatted(chosenLetters, True)
+print("\n")
 
-while (numOfAttempts <= maxAttempts):
+guessedLetters = initializeGuessedWord(lenChosenWord)
+printWordFormatted(guessedLetters)
+
+guessList = []
+
+while (numOfAttempts < maxAttempts):
 	guessed = False
-	inputLetter = input("Input a letter: ")
-	if (inputLetter in guessedWordList):
+	inputLetter = input("\nInput a letter: ").upper()
+	if (inputLetter in guessedLetters):
 		print("You have already chosen this letter. Choose a different one")
 		continue
-	for i in range(0,len(chosenWordList)):
-		if (chosenWordList[i] == inputLetter):
-			guessedWordList[i] = inputLetter
+	guessList.append(inputLetter)
+	for i in range(0,lenChosenWord):
+		# if letter not guessed yet, see it exists in chosen word
+		if (guessedLetters[i] == '_' and chosenLetters[i] == inputLetter):
+			guessedLetters[i] = inputLetter
 			guessed = True
-	if (chosenWordList == guessedWordList):
+	if (chosenLetters == guessedLetters):
 		answer = True
 		break
 	if (not guessed):
 		numOfAttempts += 1
+	printWordFormatted(guessedLetters)
+	print("\nAttempts remaining: ", (maxAttempts - numOfAttempts))
+	print("Guessed letters so far", guessList)
 
+print("********************************")
 print("Chosen word was:", chosenWord)
-print("You guessed:    ", ''.join(map(str, guessedWordList)))
+print("You guessed:    ", ''.join(map(str, guessedLetters)))
+print("********************************")
 
 if (answer):
 	print("Congratulations! You guessed the word correctly.")
