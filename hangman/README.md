b17f5f212e5a366a98e7909f5e875cdbc33cf299
Navin Kumar <kumarn@advisory.com>
2017-03-05 02:26:57 -0500

    added kid/adult mode; comments and readme file

---
+++ hangman/README.md
@@ -0,0 +1,32 @@
+## Synopsis
+
+This is popular Hangman game. It invokes REST API to download a word dictionary, picks up a word and then lets player guess the work correctly.
+Player can enter the game in either kid or adult mode. Kid mode generates smaller words (3 o4 4 letters).
+
+## Code Example
+
+
+## Motivation
+
+Fun game to be pythonic
+
+## Installation
+Download and run locally. You need to have requests API. Depending which version of Python you are using, you will have to install the module.
+E.g. download and install pip first (if not yet installed):
+$ curl -O https://bootstrap.pypa.io/get-pip.py
+$ sudo python3 get-pip.py
+or
+$ sudo easy_install pip
+After installing pip, install requests module.
+$ pip install requests //python 2
+$ pip3 install requests //python 3
+
+
+
+## API Reference
+
+requests API invokes REST API. Data comes in bytes and is decoded to UTF-8.
+
+
+## License
+Navin Kumar
--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,67 +1,88 @@
+# Author: Navin Kumar
+
 import random
 import requests
 import sys
 
-def chooseAWord():
+
+# This function invokes a REST API call to get a list of words.
+# A random word is then picked up for the game.
+def chooseAWord(mode="ADULT"):
 	wordSite = "https://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
 	response = requests.get(wordSite)
 	wordDictionary = response.content.decode('UTF-8').splitlines()
 	print(wordDictionary)
 
-	chosenWord = random.choice(wordDictionary).upper()
-	#print("Word randomly generated for the game is:", chosenWord)
-	return chosenWord
+	# Kid mode gets words with 4 or less characters :)
+	if (mode == "KID"):
+		wordDictionary = [word for word in wordDictionary if len(word) in range(3, 4)]
+	word = random.choice(wordDictionary).upper()
+	# print("Word randomly generated for the game is:", chosenWord)
+	return word
 
+
+# This function initializes player's guessed word with underscores.
 def initializeGuessedWord(lenWord):
 	guessedLetters = ["_"] * lenWord
 	return guessedLetters
 
+
+# This function prints the word.
+# optional boolean doMask is passed as True for chosen word so we don't display actual word.
+# sys.stdout.write is used to print the letters on the same line without newline break.
 def printWordFormatted(letters, doMask=False):
 	print("********************************")
 	sys.stdout.write("Chosen word: " if doMask else "Guessed word: ")
 	for letter in letters:
-		#print("letter:", letter)
+		# print("letter:", letter)
 		sys.stdout.write('X' if doMask else letter)
 
 
-###########################
-### GAME STARTS HERE ######
-###########################
+# **************** #
+# GAME STARTS HERE #
+# **************** #
 
 print("Welcome to Hangman game!!!")
 
+mode = input("Pick the mode (kid or adult): ").upper()
+
+# Initialize variables
 maxAttempts = 6
 numOfAttempts = 0
-answer = False
+answered = False
 print("You will have total " + str(maxAttempts) + " attempts to correctly guess the word.")
 
-chosenWord = chooseAWord()
+# Choose a word
+chosenWord = chooseAWord(mode)
 chosenLetters = list(chosenWord)
 lenChosenWord = len(chosenWord)
 printWordFormatted(chosenLetters, True)
 print("\n")
 
+# Initialize guessed word
 guessedLetters = initializeGuessedWord(lenChosenWord)
 printWordFormatted(guessedLetters)
 
+# Maintain the list of all guessed letters (matched or unmatched)
 guessList = []
 
-while (numOfAttempts < maxAttempts):
-	guessed = False
+# Loop while player has some attempts remaining
+while (numOfAttempts < maxAttempts and
+		   not answered):
+	correctGuess = False
 	inputLetter = input("\nInput a letter: ").upper()
-	if (inputLetter in guessedLetters):
+	if (inputLetter in guessList):
 		print("You have already chosen this letter. Choose a different one")
 		continue
 	guessList.append(inputLetter)
-	for i in range(0,lenChosenWord):
+	for i in range(0, lenChosenWord):
 		# if letter not guessed yet, see it exists in chosen word
-		if (guessedLetters[i] == '_' and chosenLetters[i] == inputLetter):
+		if (guessedLetters[i] == '_' and
+					chosenLetters[i] == inputLetter):
 			guessedLetters[i] = inputLetter
-			guessed = True
-	if (chosenLetters == guessedLetters):
-		answer = True
-		break
-	if (not guessed):
+			correctGuess = True
+	answered = (chosenLetters == guessedLetters)
+	if not correctGuess:
 		numOfAttempts += 1
 	printWordFormatted(guessedLetters)
 	print("\nAttempts remaining: ", (maxAttempts - numOfAttempts))
@@ -72,12 +93,10 @@ print("Chosen word was:", chosenWord)
 print("You guessed:    ", ''.join(map(str, guessedLetters)))
 print("********************************")
 
-if (answer):
+if answered:
 	print("Congratulations! You guessed the word correctly.")
 else:
 	print("You exceeded number of attempts. Better luck next time!")
 
-# keep guessed string guessedWord
-# cannot guess same letter again
 
 # end of file
